f3358b2ae175336202ebf39f3b1de2451cf234d7
qarnet <thomaskirschner85@yahoo.de>
2022-08-26 18:01:41 +0200

    Split up Source into Header and Source

--- Rest_API.cpp
+++ Rest_API.cpp
@@ -2,14 +2,15 @@
 #include <curl/curl.h>
 #include <fstream>
 #include <string>
+#include "Rest_API.h"
 
-class HomeAssistant{
-private:
-    std::string haIpPort, entityID, token;
+//class HomeAssistant{
+//private:
+//    std::string haIpPort, entityID, token;
 
-public:
+//public:
     //Constructor, will read out the port/ip of home-assistant and the token from the secrets file
-    HomeAssistant(){
+    HomeAssistant::HomeAssistant(){
         std::string searchHaIpPort = "HAIPPORT";
         std::string searchToken = "TOKEN";
         std::string currentLine;
@@ -35,7 +36,7 @@ public:
     }
 
     //GET - /api/
-    void GET_api(){
+    void HomeAssistant::GET_api(){
         CURL *curl;
         struct curl_slist *list = NULL;
         CURLcode res;
@@ -59,7 +60,7 @@ public:
     }
 
     //GET - /api/config
-    void GET_api_config(){
+    void HomeAssistant::GET_api_config(){
         CURL *curl;
         struct curl_slist *list = NULL;
         CURLcode res;
@@ -83,7 +84,7 @@ public:
     }
 
     //GET - /api/events
-    void GET_api_events(){
+    void HomeAssistant::GET_api_events(){
         CURL *curl;
         struct curl_slist *list = NULL;
         CURLcode res;
@@ -107,7 +108,7 @@ public:
     }
 
     //GET - /api/services
-    void GET_api_services(){
+    void HomeAssistant::GET_api_services(){
         CURL *curl;
         struct curl_slist *list = NULL;
         CURLcode res;
@@ -133,12 +134,12 @@ public:
     //GET - /api/history/period/<timestamp>
     //No arguments defaults to 1 day before the time of the request
     //NOT DONE
-    void GET_api_history_period(std::string timestamp = "", //<timestamp> with format: YYYY-MM-DDThh:mm:ssTZD (E.G. 2022-06-11T00:00:00+00:00)
-                                std::string filter_entity_id = "", //<entity_ids> with format: domain.service (E.G. sensor.weather_temperature)
-                                std::string end_time = "", //<timestamp> with format: YYYY-MM-DDThh:mm:ssTZD (defaults to 1 day)
-                                std::string minimal_response = "", //"minimal_response" or empty
-                                std::string no_attributes = "", //"no_attributes" or empty
-                                std::string significant_changes_only = "" //"significant_changes_only" or empty
+    void HomeAssistant::GET_api_history_period(std::string timestamp, //<timestamp> with format: YYYY-MM-DDThh:mm:ssTZD (E.G. 2022-06-11T00:00:00+00:00)
+                                std::string filter_entity_id, //<entity_ids> with format: domain.service (E.G. sensor.weather_temperature)
+                                std::string end_time, //<timestamp> with format: YYYY-MM-DDThh:mm:ssTZD (defaults to 1 day)
+                                std::string minimal_response, //"minimal_response" or empty
+                                std::string no_attributes, //"no_attributes" or empty
+                                std::string significant_changes_only //"significant_changes_only" or empty
                                         ){
         CURL *curl;
         struct curl_slist *list = NULL;
@@ -179,9 +180,9 @@ public:
 
 
     //GET - /api/logbook/<timestamp>
-    void GET_api_logbook(std::string timestamp = "",
-                         std::string entity = "",
-                         std::string end_time = ""
+    void HomeAssistant::GET_api_logbook(std::string timestamp,
+                         std::string entity,
+                         std::string end_time
                                  ){
         CURL *curl;
         struct curl_slist *list = NULL;
@@ -215,7 +216,7 @@ public:
     }
 
     //GET - /api/states
-    void GET_api_states(){
+    void HomeAssistant::GET_api_states(){
         CURL *curl;
         struct curl_slist *list = NULL;
         CURLcode res;
@@ -239,7 +240,7 @@ public:
     }
 
     //GET - /api/states/<entity_id>
-    void GET_api_states(std::string entity_id){ //E.G. sensor.kitchen_temperature
+    void HomeAssistant::GET_api_states(std::string entity_id){ //E.G. sensor.kitchen_temperature
         CURL *curl;
         struct curl_slist *list = NULL;
         CURLcode res;
@@ -263,7 +264,7 @@ public:
     }
 
     //GET - /api/error_log
-    void GET_api_error_log(){
+    void HomeAssistant::GET_api_error_log(){
         CURL *curl;
         struct curl_slist *list = NULL;
         CURLcode res;
@@ -287,7 +288,7 @@ public:
     }
 
     //GET - /api/camera_proxy/<camera entity_id>
-    void GET_api_camera_proxy(std::string camera_entity_id){ //E.G. camera.my_sample_camera
+    void HomeAssistant::GET_api_camera_proxy(std::string camera_entity_id){ //E.G. camera.my_sample_camera
         CURL *curl;
         struct curl_slist *list = NULL;
         CURLcode res;
@@ -311,7 +312,7 @@ public:
     }
 
     //GET - /api/calendars
-    void GET_api_calendars(){
+    void HomeAssistant::GET_api_calendars(){
         CURL *curl;
         struct curl_slist *list = NULL;
         CURLcode res;
@@ -335,9 +336,9 @@ public:
     }
 
     //GET - /api/calendars/<calendar entity_id>
-    void GET_api_calendars(std::string calendar_entity_id = "", //E.G. calendar.holidays
-                           std::string start = "", //dateTime or date
-                           std::string end = "" //dateTime or date
+    void HomeAssistant::GET_api_calendars(std::string calendar_entity_id, //E.G. calendar.holidays
+                           std::string start, //dateTime or date
+                           std::string end //dateTime or date
                                    ){
         CURL *curl;
         struct curl_slist *list = NULL;
@@ -362,7 +363,7 @@ public:
     }
 
     //POST - /api/states/<entity_id>
-    void POST_api_states(std::string entity_id,
+    void HomeAssistant::POST_api_states(std::string entity_id,
                          std::string data //E.G. "state": "25", "attributes": {"unit_of_measurement": "°C"}
                          ){
         CURL *curl;
@@ -390,7 +391,7 @@ public:
 
     //POST - /api/events/<event_type>
     //NOT DONE!!!!!!!!!
-    void POST_api_events(std::string event_type){
+    void HomeAssistant::POST_api_events(std::string event_type){
         CURL *curl;
         struct curl_slist *list = NULL;
         CURLcode res;
@@ -415,7 +416,7 @@ public:
     }
 
     //POST - /api/services/<domain>/<services>
-    void domainTurnOn(std::string domain, std::string service){
+    void HomeAssistant::domainTurnOn(std::string domain, std::string service){
         CURL *curl;
         struct curl_slist *list = NULL;
         CURLcode res;
@@ -439,7 +440,7 @@ public:
     }
 
     //POST - /api/services/<domain>/<services>
-    void domainTurnOff(std::string domain, std::string service){
+    void HomeAssistant::domainTurnOff(std::string domain, std::string service){
         CURL *curl;
         struct curl_slist *list = NULL;
         CURLcode res;
@@ -463,7 +464,7 @@ public:
     }
 
     //POST - /api/services/<domain>/<services>
-    void mqttPublish(std::string payload, std::string topic, std::string retain){
+    void HomeAssistant::mqttPublish(std::string payload, std::string topic, std::string retain){
         CURL *curl;
         struct curl_slist *list = NULL;
         CURLcode res;
@@ -488,7 +489,7 @@ public:
     }
 
     //POST - /api/template
-    void POST_api_template(std::string data){
+    void HomeAssistant::POST_api_template(std::string data){
         CURL *curl;
         struct curl_slist *list = NULL;
         CURLcode res;
@@ -513,7 +514,7 @@ public:
     }
 
     //POST - /api/config/core/check_config
-    void POST_api_config_core_check_config(){
+    void HomeAssistant::POST_api_config_core_check_config(){
         CURL *curl;
         struct curl_slist *list = NULL;
         CURLcode res;
@@ -538,7 +539,7 @@ public:
     }
 
     //POST - /api/intent/handle
-    void POST_api_intent_handle(std::string data){
+    void HomeAssistant::POST_api_intent_handle(std::string data){
         CURL *curl;
         struct curl_slist *list = NULL;
         CURLcode res;
@@ -561,17 +562,5 @@ public:
         }
         curl_global_cleanup();
     }
-};
+//};
 
-int main() {
-    HomeAssistant ha;
-    //ha.domainTurnOff("switch", "thomas_fernseher");
-    //ha.GET_api();
-    //ha.GET_api_config();
-    //ha.GET_api_events();
-    //ha.GET_api_states();
-    //ha.GET_api_error_log();
-    //ha.GET_api_calendars();
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
--- Rest_API.h
+++ Rest_API.h
@@ -1,8 +1,48 @@
 //
 // Created by thomas on 26/08/22.
 //
-
+#include <string>
 #ifndef HOME_ASSITANT_REST_API_REST_API_H
 #define HOME_ASSITANT_REST_API_REST_API_H
+class HomeAssistant{
+private:
+    std::string haIpPort, entityID, token;
+public:
+    HomeAssistant();
+    void GET_api();
+    void GET_api_config();
+    void GET_api_events();
+    void GET_api_services();
+    void GET_api_history_period(std::string timestamp = "", //<timestamp> with format: YYYY-MM-DDThh:mm:ssTZD (E.G. 2022-06-11T00:00:00+00:00)
+                                std::string filter_entity_id = "", //<entity_ids> with format: domain.service (E.G. sensor.weather_temperature)
+                                std::string end_time = "", //<timestamp> with format: YYYY-MM-DDThh:mm:ssTZD (defaults to 1 day)
+                                std::string minimal_response = "", //"minimal_response" or empty
+                                std::string no_attributes = "", //"no_attributes" or empty
+                                std::string significant_changes_only = "" //"significant_changes_only" or empty
+                                );
+    void GET_api_logbook(std::string timestamp = "",
+                         std::string entity = "",
+                         std::string end_time = ""
+    );
+    void GET_api_states();
+    void GET_api_states(std::string /*entity_id*/);
+    void GET_api_error_log();
+    void GET_api_camera_proxy(std::string /*camera_entity_id*/);
+    void GET_api_calendars();
+    void GET_api_calendars(std::string calendar_entity_id = "", //E.G. calendar.holidays
+                           std::string start = "", //dateTime or date
+                           std::string end = "" //dateTime or date
+    );
+    void POST_api_states(std::string /*entity_id*/,
+                         std::string /*data*/ //E.G. "state": "25", "attributes": {"unit_of_measurement": "°C"}
+    );
+    void POST_api_events(std::string /*event_type*/);
+    void domainTurnOn(std::string /*domain*/, std::string /*service*/);
+    void domainTurnOff(std::string /*domain*/, std::string /*service*/);
+    void mqttPublish(std::string /*payload*/, std::string /*topic*/, std::string /*retain*/);
+    void POST_api_template(std::string /*data*/);
+    void POST_api_config_core_check_config();
+    void POST_api_intent_handle(std::string /*data*/);
 
+};
 #endif //HOME_ASSITANT_REST_API_REST_API_H
