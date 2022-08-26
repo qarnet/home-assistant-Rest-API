1426370fdfdf9a94b6e7ae112fa81464873c3e27
qarnet <thomaskirschner85@yahoo.de>
2022-08-26 16:56:28 +0200

    Almost fully configured all methods. Not tested functionality yet!

--- Rest_API.cpp
+++ Rest_API.cpp
@@ -2,8 +2,6 @@
 #include <curl/curl.h>
 #include <fstream>
 #include <string>
-#include "Rest_API.h"
-#include <filesystem>
 
 class HomeAssistant{
 private:
@@ -133,13 +131,87 @@ public:
     }
 
     //GET - /api/history/period/<timestamp>
-    void GET_api_history_period(std::string timestamp){
+    //No arguments defaults to 1 day before the time of the request
+    //NOT DONE
+    void GET_api_history_period(std::string timestamp = "", //<timestamp> with format: YYYY-MM-DDThh:mm:ssTZD (E.G. 2022-06-11T00:00:00+00:00)
+                                std::string filter_entity_id = "", //<entity_ids> with format: domain.service (E.G. sensor.weather_temperature)
+                                std::string end_time = "", //<timestamp> with format: YYYY-MM-DDThh:mm:ssTZD (defaults to 1 day)
+                                std::string minimal_response = "", //"minimal_response" or empty
+                                std::string no_attributes = "", //"no_attributes" or empty
+                                std::string significant_changes_only = "" //"significant_changes_only" or empty
+                                        ){
+        CURL *curl;
+        struct curl_slist *list = NULL;
+        CURLcode res;
+        std::string url = "http://" + haIpPort + "/api/history/period/" + timestamp;
+        if(!filter_entity_id.empty()){
 
+        }
+        else if(!end_time.empty()){
+
+        }
+        else if(!minimal_response.empty()){
+
+        }
+        else if(!no_attributes.empty()){
+
+        }
+        else if(!significant_changes_only.empty()){
+
+        }
+        std::string auth = "Authorization: Bearer " + token;
+
+        curl = curl_easy_init();
+        if(curl) {
+            curl_easy_setopt(curl, CURLOPT_URL, &url[0]);
+            //curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L); //Follow redirects
+            list = curl_slist_append(list, &auth[0]);
+            list = curl_slist_append(list, "Content-Type: application/json");
+            curl_easy_setopt(curl, CURLOPT_HTTPHEADER, list);
+            res = curl_easy_perform(curl);
+            if(res != CURLE_OK)
+                fprintf(stderr, "curl_easy_perform() failed");
+            std::cout << res;
+            curl_easy_cleanup(curl);
+        }
+        return;
     }
 
-    //GET - /api/logbook/<timestamp>
-    void GET_api_logbook(std::string timestamp){
 
+    //GET - /api/logbook/<timestamp>
+    void GET_api_logbook(std::string timestamp = "",
+                         std::string entity = "",
+                         std::string end_time = ""
+                                 ){
+        CURL *curl;
+        struct curl_slist *list = NULL;
+        CURLcode res;
+        std::string url = "http://" + haIpPort + "/api/logbook/";
+        if(!timestamp.empty()){
+
+        }
+        else if(!entity.empty()){
+
+        }
+        else if(!end_time.empty()){
+
+        }
+        std::string auth = "Authorization: Bearer " + token;
+
+        curl = curl_easy_init();
+        if(curl) {
+            curl_easy_setopt(curl, CURLOPT_URL, &url[0]);
+            //curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L); //Follow redirects
+            list = curl_slist_append(list, &auth[0]);
+            list = curl_slist_append(list, "Content-Type: application/json");
+            curl_easy_setopt(curl, CURLOPT_HTTPHEADER, list);
+            res = curl_easy_perform(curl);
+            if(res != CURLE_OK)
+                fprintf(stderr, "curl_easy_perform() failed");
+            std::cout << res;
+            curl_easy_cleanup(curl);
+        }
+        return;
     }
 
     //GET - /api/states
@@ -167,8 +239,27 @@ public:
     }
 
     //GET - /api/states/<entity_id>
-    void GET_api_states(std::string entity_id){
+    void GET_api_states(std::string entity_id){ //E.G. sensor.kitchen_temperature
+        CURL *curl;
+        struct curl_slist *list = NULL;
+        CURLcode res;
+        std::string url = "http://" + haIpPort + "/api/states/" + entity_id;
+        std::string auth = "Authorization: Bearer " + token;
 
+        curl = curl_easy_init();
+        if(curl) {
+            curl_easy_setopt(curl, CURLOPT_URL, &url[0]);
+            //curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L); //Follow redirects
+            list = curl_slist_append(list, &auth[0]);
+            list = curl_slist_append(list, "Content-Type: application/json");
+            curl_easy_setopt(curl, CURLOPT_HTTPHEADER, list);
+            res = curl_easy_perform(curl);
+            if(res != CURLE_OK)
+                fprintf(stderr, "curl_easy_perform() failed");
+            std::cout << res;
+            curl_easy_cleanup(curl);
+        }
+        return;
     }
 
     //GET - /api/error_log
@@ -196,8 +287,27 @@ public:
     }
 
     //GET - /api/camera_proxy/<camera entity_id>
-    void GET_api_camera_proxy(std::string camera_entity_id){
+    void GET_api_camera_proxy(std::string camera_entity_id){ //E.G. camera.my_sample_camera
+        CURL *curl;
+        struct curl_slist *list = NULL;
+        CURLcode res;
+        std::string url = "http://" + haIpPort + "/api/camera_proxy/" + camera_entity_id;
+        std::string auth = "Authorization: Bearer " + token;
 
+        curl = curl_easy_init();
+        if(curl) {
+            curl_easy_setopt(curl, CURLOPT_URL, &url[0]);
+            //curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L); //Follow redirects
+            list = curl_slist_append(list, &auth[0]);
+            list = curl_slist_append(list, "Content-Type: application/json");
+            curl_easy_setopt(curl, CURLOPT_HTTPHEADER, list);
+            res = curl_easy_perform(curl);
+            if(res != CURLE_OK)
+                fprintf(stderr, "curl_easy_perform() failed");
+            std::cout << res;
+            curl_easy_cleanup(curl);
+        }
+        return;
     }
 
     //GET - /api/calendars
@@ -225,18 +335,83 @@ public:
     }
 
     //GET - /api/calendars/<calendar entity_id>
-    void GET_api_calendars(std::string calendar_entity_id){
+    void GET_api_calendars(std::string calendar_entity_id = "", //E.G. calendar.holidays
+                           std::string start = "", //dateTime or date
+                           std::string end = "" //dateTime or date
+                                   ){
+        CURL *curl;
+        struct curl_slist *list = NULL;
+        CURLcode res;
+        std::string url = "http://" + haIpPort + "/api/calendars/";
+        std::string auth = "Authorization: Bearer " + token;
 
+        curl = curl_easy_init();
+        if(curl) {
+            curl_easy_setopt(curl, CURLOPT_URL, &url[0]);
+            //curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L); //Follow redirects
+            list = curl_slist_append(list, &auth[0]);
+            list = curl_slist_append(list, "Content-Type: application/json");
+            curl_easy_setopt(curl, CURLOPT_HTTPHEADER, list);
+            res = curl_easy_perform(curl);
+            if(res != CURLE_OK)
+                fprintf(stderr, "curl_easy_perform() failed");
+            std::cout << res;
+            curl_easy_cleanup(curl);
+        }
+        return;
     }
 
     //POST - /api/states/<entity_id>
-    void POST_api_states(std::string entity_id){
+    void POST_api_states(std::string entity_id,
+                         std::string data //E.G. "state": "25", "attributes": {"unit_of_measurement": "°C"}
+                         ){
+        CURL *curl;
+        struct curl_slist *list = NULL;
+        CURLcode res;
+        std::string url = "http://" + haIpPort + "/api/states/"+ entity_id;
+        //std::string postfields = "{\"entity_id\": \"" + domain + "." + service + "\"}";
+        std::string postfields = "{" + data + "}";
+        std::string auth = "Authorization: Bearer " + token;
 
+        curl = curl_easy_init();
+        if(curl){
+            curl_easy_setopt(curl, CURLOPT_URL, &url[0]);
+            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, &postfields[0]);
+            list = curl_slist_append(list, &auth[0]);
+            list = curl_slist_append(list, "Content-Type: application/json");
+            curl_easy_setopt(curl, CURLOPT_HTTPHEADER, list);
+            res = curl_easy_perform(curl);
+            if(res != CURLE_OK)
+                fprintf(stderr, "curl_easy_perform() failed");
+            curl_easy_cleanup(curl);
+        }
+        curl_global_cleanup();
     }
 
     //POST - /api/events/<event_type>
+    //NOT DONE!!!!!!!!!
     void POST_api_events(std::string event_type){
+        CURL *curl;
+        struct curl_slist *list = NULL;
+        CURLcode res;
+        std::string url = "http://" + haIpPort + "/api/events/"+ event_type;
+        //std::string postfields = "{\"entity_id\": \"" + domain + "." + service + "\"}";
+        //no postfields needed?
+        std::string auth = "Authorization: Bearer " + token;
 
+        curl = curl_easy_init();
+        if(curl){
+            curl_easy_setopt(curl, CURLOPT_URL, &url[0]);
+            //curl_easy_setopt(curl, CURLOPT_POSTFIELDS, &postfields[0]);
+            list = curl_slist_append(list, &auth[0]);
+            list = curl_slist_append(list, "Content-Type: application/json");
+            curl_easy_setopt(curl, CURLOPT_HTTPHEADER, list);
+            res = curl_easy_perform(curl);
+            if(res != CURLE_OK)
+                fprintf(stderr, "curl_easy_perform() failed");
+            curl_easy_cleanup(curl);
+        }
+        curl_global_cleanup();
     }
 
     //POST - /api/services/<domain>/<services>
@@ -313,18 +488,78 @@ public:
     }
 
     //POST - /api/template
-    void POST_api_template(){
+    void POST_api_template(std::string data){
+        CURL *curl;
+        struct curl_slist *list = NULL;
+        CURLcode res;
+        std::string url = "http://" + haIpPort + "/api/template";
+        //std::string postfields = "{\"entity_id\": \"" + domain + "." + service + "\"}";
+        std::string postfields = "{" + data + "}";
+        std::string auth = "Authorization: Bearer " + token;
 
+        curl = curl_easy_init();
+        if(curl){
+            curl_easy_setopt(curl, CURLOPT_URL, &url[0]);
+            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, &postfields[0]);
+            list = curl_slist_append(list, &auth[0]);
+            list = curl_slist_append(list, "Content-Type: application/json");
+            curl_easy_setopt(curl, CURLOPT_HTTPHEADER, list);
+            res = curl_easy_perform(curl);
+            if(res != CURLE_OK)
+                fprintf(stderr, "curl_easy_perform() failed");
+            curl_easy_cleanup(curl);
+        }
+        curl_global_cleanup();
     }
 
     //POST - /api/config/core/check_config
     void POST_api_config_core_check_config(){
+        CURL *curl;
+        struct curl_slist *list = NULL;
+        CURLcode res;
+        std::string url = "http://" + haIpPort + "/api/config/core/check_config";
+        //std::string postfields = "{\"entity_id\": \"" + domain + "." + service + "\"}";
+        //no postfields needed????
+        std::string auth = "Authorization: Bearer " + token;
 
+        curl = curl_easy_init();
+        if(curl){
+            curl_easy_setopt(curl, CURLOPT_URL, &url[0]);
+            //curl_easy_setopt(curl, CURLOPT_POSTFIELDS, &postfields[0]);
+            list = curl_slist_append(list, &auth[0]);
+            list = curl_slist_append(list, "Content-Type: application/json");
+            curl_easy_setopt(curl, CURLOPT_HTTPHEADER, list);
+            res = curl_easy_perform(curl);
+            if(res != CURLE_OK)
+                fprintf(stderr, "curl_easy_perform() failed");
+            curl_easy_cleanup(curl);
+        }
+        curl_global_cleanup();
     }
 
     //POST - /api/intent/handle
-    void POST_api_intent_handle(){
+    void POST_api_intent_handle(std::string data){
+        CURL *curl;
+        struct curl_slist *list = NULL;
+        CURLcode res;
+        std::string url = "http://" + haIpPort + "api/intent/handle";
+        //std::string postfields = "{\"entity_id\": \"" + domain + "." + service + "\"}";
+        std::string postfields = "{" + data + "}";
+        std::string auth = "Authorization: Bearer " + token;
 
+        curl = curl_easy_init();
+        if(curl){
+            curl_easy_setopt(curl, CURLOPT_URL, &url[0]);
+            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, &postfields[0]);
+            list = curl_slist_append(list, &auth[0]);
+            list = curl_slist_append(list, "Content-Type: application/json");
+            curl_easy_setopt(curl, CURLOPT_HTTPHEADER, list);
+            res = curl_easy_perform(curl);
+            if(res != CURLE_OK)
+                fprintf(stderr, "curl_easy_perform() failed");
+            curl_easy_cleanup(curl);
+        }
+        curl_global_cleanup();
     }
 };
 
